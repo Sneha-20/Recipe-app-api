fcf92d5376af661d5f386143077d2ff755950713
sneha <sneha.saurav20@gmail.comm>
2022-12-02 22:39:49 +0530

    email address normalize

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,6 +20,20 @@ class ModelsTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    
+    def test_user_email_normalize(self):
+        sample_test =[
+            ['test20@EXAMPLE.COM', 'test2-@example.com'],
+            ['Test20@Example.com', 'Test20@example.com'],
+            ['TEST2@EXAMPLE.COM', 'TEST2@example.com'],
+            ['test4@example.com', 'test4@example.com']
+        ]
+
+        for email , expected in sample_test:
+            user = get_user_model.objects.create_user(email, 'Test@123')
+            self.assertEqual(user.email, expected)
+
         
 
 
